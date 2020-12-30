bcf4a8c6aa91da30ba1409fe29d0be650ca9cffd
Samvw90 <45743681+Samvw90@users.noreply.github.com>
2020-12-29 18:32:40 -0800

    Logo to svg

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -2,6 +2,11 @@ import React from "react"
 import { HeroWrapper } from "../elements"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
+import CSS from "../images/CSS3_logo_and_wordmark.svg"
+import HTML from "../images/HTML5_Logo.svg"
+import JS from "../images/javascript.svg"
+import SVG from "../images/1logo.svg"
+import SVG2 from "../images/logo.svg"
 
 const HeroBanner = () => {
   const data = useStaticQuery(graphql`
@@ -21,6 +26,15 @@ const HeroBanner = () => {
         fluid={data.placeholderImage.childImageSharp.fluid}
         style={{ height: "25rem" }}
       />
+      <div
+        style={{ display: "flex", border: "red solid 1px", height: "18rem" }}
+      >
+        <img src={CSS} alt="" />
+        <img src={HTML} alt="" />
+        <img src={JS} alt="" />
+        <img src={SVG} alt="" />
+        <img src={SVG2} alt="" />
+      </div>
     </HeroWrapper>
   )
 }
